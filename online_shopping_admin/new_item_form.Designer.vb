443b2e7f36a41a1b7d31e7b03e598bb204cb77c9
tmkoski <tmkoski69@gmail.com>
2022-12-31 10:34:14 -0800

    new_item_form design complete

--- online_shopping_admin/new_item_form.Designer.vb
+++ online_shopping_admin/new_item_form.Designer.vb
@@ -38,98 +38,109 @@ Partial Class new_item_form
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(20, 32)
+        Me.Label1.Location = New System.Drawing.Point(12, 20)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(92, 41)
+        Me.Label1.Size = New System.Drawing.Size(56, 25)
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "name"
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(20, 110)
+        Me.Label2.Location = New System.Drawing.Point(12, 67)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(134, 41)
+        Me.Label2.Size = New System.Drawing.Size(81, 25)
         Me.Label2.TabIndex = 1
         Me.Label2.Text = "category"
         '
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(20, 188)
+        Me.Label3.Location = New System.Drawing.Point(12, 115)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(83, 41)
+        Me.Label3.Size = New System.Drawing.Size(50, 25)
         Me.Label3.TabIndex = 2
         Me.Label3.Text = "price"
         '
         'Label4
         '
         Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(20, 257)
+        Me.Label4.Location = New System.Drawing.Point(12, 157)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(224, 41)
+        Me.Label4.Size = New System.Drawing.Size(134, 25)
         Me.Label4.TabIndex = 3
         Me.Label4.Text = "initital_on-hand"
         '
         'Label5
         '
         Me.Label5.AutoSize = True
-        Me.Label5.Location = New System.Drawing.Point(20, 327)
+        Me.Label5.Location = New System.Drawing.Point(12, 199)
+        Me.Label5.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(193, 41)
+        Me.Label5.Size = New System.Drawing.Size(116, 25)
         Me.Label5.TabIndex = 4
         Me.Label5.Text = "case quantity"
         '
         'item_name_entry
         '
-        Me.item_name_entry.Location = New System.Drawing.Point(154, 32)
+        Me.item_name_entry.Location = New System.Drawing.Point(91, 20)
+        Me.item_name_entry.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.item_name_entry.Name = "item_name_entry"
-        Me.item_name_entry.Size = New System.Drawing.Size(250, 47)
+        Me.item_name_entry.Size = New System.Drawing.Size(149, 31)
         Me.item_name_entry.TabIndex = 5
         '
         'category_entry
         '
-        Me.category_entry.Location = New System.Drawing.Point(193, 108)
+        Me.category_entry.Location = New System.Drawing.Point(114, 66)
+        Me.category_entry.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.category_entry.Name = "category_entry"
-        Me.category_entry.Size = New System.Drawing.Size(250, 47)
+        Me.category_entry.Size = New System.Drawing.Size(149, 31)
         Me.category_entry.TabIndex = 6
         '
         'price_entry
         '
-        Me.price_entry.Location = New System.Drawing.Point(145, 191)
+        Me.price_entry.Location = New System.Drawing.Point(85, 116)
+        Me.price_entry.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.price_entry.Name = "price_entry"
-        Me.price_entry.Size = New System.Drawing.Size(250, 47)
+        Me.price_entry.Size = New System.Drawing.Size(149, 31)
         Me.price_entry.TabIndex = 7
         '
         'on_hand_entry
         '
-        Me.on_hand_entry.Location = New System.Drawing.Point(262, 259)
+        Me.on_hand_entry.Location = New System.Drawing.Point(154, 158)
+        Me.on_hand_entry.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.on_hand_entry.Name = "on_hand_entry"
-        Me.on_hand_entry.Size = New System.Drawing.Size(250, 47)
+        Me.on_hand_entry.Size = New System.Drawing.Size(149, 31)
         Me.on_hand_entry.TabIndex = 8
         '
         'case_quantity_entry
         '
-        Me.case_quantity_entry.Location = New System.Drawing.Point(250, 327)
+        Me.case_quantity_entry.Location = New System.Drawing.Point(147, 199)
+        Me.case_quantity_entry.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.case_quantity_entry.Name = "case_quantity_entry"
-        Me.case_quantity_entry.Size = New System.Drawing.Size(250, 47)
+        Me.case_quantity_entry.Size = New System.Drawing.Size(149, 31)
         Me.case_quantity_entry.TabIndex = 9
         '
         'add_item_button
         '
         Me.add_item_button.AutoSize = True
-        Me.add_item_button.Location = New System.Drawing.Point(279, 388)
+        Me.add_item_button.Location = New System.Drawing.Point(164, 237)
+        Me.add_item_button.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.add_item_button.Name = "add_item_button"
-        Me.add_item_button.Size = New System.Drawing.Size(267, 58)
+        Me.add_item_button.Size = New System.Drawing.Size(165, 35)
         Me.add_item_button.TabIndex = 10
         Me.add_item_button.Text = "add to warehouse"
         Me.add_item_button.UseVisualStyleBackColor = True
         '
         'new_item_form
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(17.0!, 41.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 25.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(471, 274)
         Me.Controls.Add(Me.add_item_button)
         Me.Controls.Add(Me.case_quantity_entry)
         Me.Controls.Add(Me.on_hand_entry)
@@ -141,6 +152,7 @@ Partial Class new_item_form
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
+        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.Name = "new_item_form"
         Me.Text = "new_item_form"
         Me.ResumeLayout(False)
