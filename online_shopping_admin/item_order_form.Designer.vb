20cf46cfc16786488bda4010ffa57cfb94493294
tmkoski <tmkoski69@gmail.com>
2023-01-05 08:45:26 -0800

    user can successfully retrieve an item id by clicking on the corresponding row in the table

--- online_shopping_admin/item_order_form.Designer.vb
+++ online_shopping_admin/item_order_form.Designer.vb
@@ -22,24 +22,24 @@ Partial Class item_order_form
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.warehouse_item_table = New System.Windows.Forms.DataGridView()
         Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.place_order_button = New System.Windows.Forms.Button()
         Me.Label1 = New System.Windows.Forms.Label()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
-        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.warehouse_item_table, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.GroupBox1.SuspendLayout()
         Me.SuspendLayout()
         '
-        'DataGridView1
+        'warehouse_item_table
         '
-        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DataGridView1.Location = New System.Drawing.Point(12, 30)
-        Me.DataGridView1.Name = "DataGridView1"
-        Me.DataGridView1.RowHeadersWidth = 62
-        Me.DataGridView1.RowTemplate.Height = 33
-        Me.DataGridView1.Size = New System.Drawing.Size(409, 382)
-        Me.DataGridView1.TabIndex = 2
+        Me.warehouse_item_table.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.warehouse_item_table.Location = New System.Drawing.Point(12, 30)
+        Me.warehouse_item_table.Name = "warehouse_item_table"
+        Me.warehouse_item_table.RowHeadersWidth = 62
+        Me.warehouse_item_table.RowTemplate.Height = 33
+        Me.warehouse_item_table.Size = New System.Drawing.Size(409, 382)
+        Me.warehouse_item_table.TabIndex = 2
         '
         'GroupBox1
         '
@@ -52,6 +52,13 @@ Partial Class item_order_form
         Me.GroupBox1.TabIndex = 3
         Me.GroupBox1.TabStop = False
         Me.GroupBox1.Text = "GroupBox1"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(137, 27)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(150, 31)
+        Me.TextBox1.TabIndex = 4
         '
         'place_order_button
         '
@@ -71,29 +78,22 @@ Partial Class item_order_form
         Me.Label1.TabIndex = 2
         Me.Label1.Text = "order quantity"
         '
-        'TextBox1
-        '
-        Me.TextBox1.Location = New System.Drawing.Point(137, 27)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(150, 31)
-        Me.TextBox1.TabIndex = 4
-        '
         'item_order_form
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 25.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
         Me.Controls.Add(Me.GroupBox1)
-        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.warehouse_item_table)
         Me.Name = "item_order_form"
         Me.Text = "item_order_form"
-        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.warehouse_item_table, System.ComponentModel.ISupportInitialize).EndInit()
         Me.GroupBox1.ResumeLayout(False)
         Me.GroupBox1.PerformLayout()
         Me.ResumeLayout(False)
 
     End Sub
-    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents warehouse_item_table As DataGridView
     Friend WithEvents GroupBox1 As GroupBox
     Friend WithEvents TextBox1 As TextBox
     Friend WithEvents place_order_button As Button
--- online_shopping_admin/item_order_form.vb
+++ online_shopping_admin/item_order_form.vb
@@ -1,14 +1,24 @@
 ﻿Public Class item_order_form
     Dim order_controller As New OrderingControls("online_shopping.db")
+    Dim current_item_id As Integer
     Private Sub item_order_form_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         'TODO: display all warehouse items in datagrid
         Dim warehouse_items As DataTable = order_controller.view_warehouse_items()
-        DataGridView1.DataSource = warehouse_items
+        warehouse_item_table.DataSource = warehouse_items
         GroupBox1.Hide()
     End Sub
 
     Private Sub place_order_button_Click(sender As Object, e As EventArgs)
         'TODO: on button click, update the PI of the item ordered
         'as quantity_ordered * case_quantity
+    End Sub
+
+    Private Sub warehouse_item_table_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles warehouse_item_table.CellContentClick
+        'allows the user to select a row in the table (represents an item).
+        'it saves the id in a variable, then uses that id when making the order call
+        Dim rowPos As Integer = e.RowIndex
+        current_item_id = warehouse_item_table.Rows(rowPos).Cells(0).Value
+        MsgBox(current_item_id
+               )
     End Sub
 End Class
